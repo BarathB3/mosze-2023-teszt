98624a60e8dc4f0f429419b674ea4ae9cbcc04f8
BarathB3 <barathbence3@gmail.com>
2024-09-13 11:04:48 +0200

    Hibák kommentálása a hazi_feladat.cpp-ben

--- hazi_feladat.cpp
+++ hazi_feladat.cpp
@@ -5,27 +5,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Az "NELEMENTS" hibásan van írva, a helyes változónév "N_ELEMENTS"
+    std::cout << '1-100 ertekek duplazasa' // Az egyes idézõjelek helytelenek. A karakterláncokat dupla idézõjelekbe kell tenni.
+    for (int i = 0;) // for (int i = 0; i < N_ELEMENTS; i++).
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // for (int i = 0; i < N_ELEMENTS; i++).
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" // pontosvesszõ hiánya
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; // Az "atlag" változó nem inicializált
+    for (int i = 0; i < N_ELEMENTS, i++) // vesszõ (,) helyett pontosvesszõ (;) kellene.
     {
-        atlag += b[i]
+        atlag += b[i] //A sor végi pontosvesszõ hiányzik.
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
+
 }
 =======
+
+
+
 #include <iostream>
 
 constexpr int N_ELEMENTS = 100;
@@ -41,7 +45,7 @@ int main()
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
-    }    
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
@@ -52,4 +56,5 @@ int main()
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
 >>>>>>> 0f09f55e49b3cd01710d23988bc0d018b6e8465d
